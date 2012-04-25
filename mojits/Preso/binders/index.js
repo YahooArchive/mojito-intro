eea366cd933ca90cbd408be5ed4dde73601e9fa5
gzip <gzip@gzipped.com>
2012-04-24 17:52:24 -0700

    Flesh out behavior and slides for Preso

--- mojits/Preso/binders/index.js
+++ mojits/Preso/binders/index.js
@@ -5,7 +5,77 @@ YUI.add('PresoBinder', function(Y, NAME)
         init: function(proxy) 
         {
             var self = this;
+            
+            // store proxy for access in other methods
             self.proxy = proxy;
+            
+            // the number of slides should be specified in application.json specs
+            self.numSlides = proxy.config.numSlides;
+        },
+        
+        bind: function(node)
+        {
+            var self = this;
+            
+            // sniff for proper key event
+            self.keyEvent = Y.UA.webkit || Y.UA.ie ? 'keydown' : 'keypress';
+            
+            // listen for keypress
+            Y.one('body').on(self.keyEvent, self.handleKey, self);
+            
+            // make sure current slide is set
+            self.onRefreshView(node);
+        },
+        
+        onRefreshView: function(node)
+        {
+            // get the current slide number from a data atribute within the view
+            this.currentSlide = parseInt(node.getAttribute('data-slide'), 10);
+            
+            // update page title
+            Y.one('title').setContent(Y.one('h1').getContent());
+        },
+        
+        changeSlide: function(num)
+        {
+            this.proxy.refreshView({params:{url:{slide: num}}});
+        },
+        
+        handleKey: function(e)
+        {
+            var self = this,
+                key = e.keyCode,
+                numSlides = self.numSlides,
+                currentSlide = self.currentSlide,
+                nextSlide = 0;
+            
+            // left, up, page up respectively
+            if(key == 37 || key == 38 || key == 33)
+            {
+                nextSlide = currentSlide > 1 ? currentSlide - 1 : numSlides;
+            }
+            // right, down, page down respectively
+            else if(key == 39 || key == 40 || key == 34)
+            {
+                nextSlide = currentSlide < numSlides ? currentSlide + 1 : 1;
+            }
+            // home
+            else if(key == 36)
+            {
+                nextSlide = 1;
+            }
+            // end
+            else if(key == 35)
+            {
+                nextSlide = self.numSlides;
+            }
+            
+            // change slide if we got an expected key
+            if(nextSlide)
+            {
+                e.preventDefault();
+                self.changeSlide(nextSlide);
+            }
         }
     };
 }, '0.0.1', {requires: ['mojito-client']});
--- mojits/Preso/controller.common.js
+++ mojits/Preso/controller.common.js
@@ -9,8 +9,44 @@ YUI.add('Preso', function(Y, NAME)
         
         index: function(ac)
         {
-            var self = this;
-            ac.done({}, {'view':{'name': 'slide1'}});
+            var self = this,
+                num = ac.params.url('slide') || '1',
+                children = null;
+            
+            // store ac for access in other methods and closure
+            self.ac = ac;
+            
+            function done(meta)
+            {
+                var viewName = 'slide' + num,
+                    meta = meta || {};
+                
+                // workaround for a diff view and binder
+                self.populateBinder('index', viewName);
+                
+                // specify the curent slide as the view
+                meta.view = {'name': viewName};
+                
+                // end execution
+                self.ac.done({}, meta);
+            }
+            
+            // handle any children that the slide expects
+            switch(num)
+            {
+                
+            }
+            
+            done();
+        },
+        
+        // use the binder from one view in another
+        populateBinder: function(base, target)
+        {
+            var self = this,
+                views = self.ac.command.instance.views;
+            
+            views[target] = Y.merge(views[base], views[target]);
         }
     };
 }, '0.0.1', {requires: ['json', 'mojito']});
