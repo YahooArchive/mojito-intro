94e6bd8238898a791dfe7d19b62db3a0fc448782
gzip <gamaiel@yahoo.com>
2012-04-28 12:31:10 -0700

    Try local mojito first

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-module.exports = require('/usr/local/lib/node_modules/mojito').createServer();
+try {
+    module.exports = require('mojito').createServer();
+} catch(e) {
+    try {
+        module.exports = require('/usr/local/lib/node_modules/mojito').createServer();
+    } catch(e) {
+        console.log(e.message);
+        console.log('Please install mojito first via npm.');
+    }
+}
